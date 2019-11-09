9ba50ff0e8fdb01c4f6577e85c9a6147736b5938
Titus Tienaah <titus.intdxdt@gmail.com>
2019-11-09 20:13:58 +0000

    flt, feq

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,61 @@
 use std::fmt::Debug;
 
 pub use num_traits as num;
+use std::ops::{AddAssign, SubAssign, MulAssign, DivAssign};
 pub use num_traits::{Bounded, Num, NumCast, Signed, Zero, One, Float, FloatConst};
 
 pub trait Numeric: Bounded + Signed + Num + NumCast + Clone + Copy + PartialOrd + Debug {}
 
 impl<T> Numeric for T where T: Bounded + Num + NumCast + Clone + Copy + Signed + PartialOrd + Debug {}
+
+///Floating point behavioural trait
+pub trait Flt: Float + FloatConst + Feq + AddAssign + SubAssign + MulAssign + DivAssign {}
+
+impl<T> Flt for T
+    where T: Float + FloatConst + Feq + AddAssign + SubAssign + MulAssign + DivAssign {}
+
+
+///Compare two floating point values
+pub trait Feq: Numeric {
+    const EPS: Self;
+    ///Compare two floating point values to nearest epsilon
+    #[inline]
+    fn feq_eps(self, other: Self, eps: Self) -> bool {
+        self == other || ((self - other).abs() < eps)
+    }
+
+    ///Compare two floating point values
+    #[inline]
+    fn feq(self, other: Self) -> bool {
+        Self::feq_eps(self, other, Self::EPS)
+    }
+}
+macro_rules! impl_feq_floats {
+    ($T:ty) => {
+        impl Feq for $T {
+            const EPS: $T = 1.0e-12;
+        }
+    };
+}
+
+macro_rules! impl_feq_ints {
+    ($T:ty) => {
+        impl Feq for $T {
+            const EPS: $T = 0;
+            #[inline]
+            fn feq_eps(self, other: Self, _: Self) -> bool {
+                self == other
+            }
+        }
+    };
+}
+
+impl_feq_floats!(f64);
+impl_feq_floats!(f32);
+
+impl_feq_ints!(i64);
+impl_feq_ints!(i32);
+
 
 #[inline]
 pub fn max<T>(a: T, b: T) -> T
